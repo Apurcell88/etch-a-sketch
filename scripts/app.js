867132db12890b27743e1cf9cb8f8c0e9ffa6196
Adam Purcell <apurcell88@gmail.com>
2021-10-11 10:15:49 -0500

    Added functions to create a grid along with a random color. Added some styles that implement the grid. This is finished.

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,39 @@
 const mainContainer = document.querySelector('#main-container');
+const button = document.querySelector('button');
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.className = 'row-color row-border row-width height flex';
-    mainContainer.appendChild(row);
+button.addEventListener('click', makeGrid);
 
-    for (let j = 0; j < 16; j++) {
-        let div = document.createElement('div');
-        div.className = 'vertical-border width';
-        row.appendChild(div);
+function generateRandomColor() {
+    const r = Math.floor(Math.random() * 255) + 1;
+    const g = Math.floor(Math.random() * 255) + 1;
+    const b = Math.floor(Math.random() * 255) + 1;
+    const color = `rgb(${r}, ${g}, ${b})`;
+
+    return color;
+}
+
+function makeGrid() {
+    mainContainer.innerHTML = '';
+    const rows = parseInt(prompt('How big is the grid going to be?'));
+    const randomColor = generateRandomColor();
+
+    if (rows < 100) {
+        
+        for (let i = 0; i < rows; i++) {
+            const row = document.createElement('div');
+            row.className = 'row-color row-border row-width height flex';
+            mainContainer.appendChild(row);
+        
+            for (let j = 0; j < rows; j++) {
+                let div = document.createElement('div');
+                div.style.width = `${1 / 4 * 100}%`;
+                div.className = 'vertical-border';
+                row.appendChild(div);
+        
+                div.addEventListener('mouseenter', (e) => {
+                    div.style.backgroundColor = randomColor;
+                });
+            }
+        }
     }
 }
